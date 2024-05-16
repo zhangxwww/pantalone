ba5469c466a75acda70b875821aa8c30aebfd8ca
zhangxwww <631133544@qq.com>
2024-05-16 19:09:08 +0800

    basic framework

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,23 +1,152 @@
 <template>
   <el-row>
-    <el-col :span="18">
+    <el-col :span="15">
       <div id="main"
            style="width: 900px; height: 300px"></div>
     </el-col>
-    <el-col :span="6">
+    <el-col :span="9">
+      <el-row>
+        <el-dropdown trigger="hover"
+                     v-on:command="onAddSelect">
+          <el-button type="primary"
+                     @click="onAddClick">
+            <el-icon>
+              <plus />
+            </el-icon>
+          </el-button>
+          <template #dropdown>
+            <el-dropdown-menu>
+              <el-dropdown-item command="1">1</el-dropdown-item>
+              <el-dropdown-item command="2">2</el-dropdown-item>
+            </el-dropdown-menu>
+          </template>
+        </el-dropdown>
+        <el-button>
+          <el-icon>
+            <download />
+          </el-icon>
+        </el-button>
+        <el-button>
+          <el-icon>
+            <upload />
+          </el-icon>
+        </el-button>
+      </el-row>
+      <el-tabs type="border-card"
+               :v-model="table_tab_name">
+        <el-tab-pane label="User">
+          <el-table :data="tableData"
+                    height="500"
+                    style="width: 100%">
+            <el-table-column prop="date"
+                             label="Date"
+                             width="180"
+                             :sortable="true" />
+            <el-table-column prop="name"
+                             label="Name"
+                             width="180" />
+            <el-table-column prop="address"
+                             label="Address" />
+            <el-table-column label="Operations">
+              <template #default="scope">
+                <el-button size="small"
+                           @click="handleEdit(scope.$index, scope.row)">
+                  Edit
+                </el-button>
+                <el-button size="small"
+                           type="danger"
+                           @click="handleDelete(scope.$index, scope.row)">
+                  Delete
+                </el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-tab-pane>
+        <el-tab-pane label="Config">Config</el-tab-pane>
+        <el-tab-pane label="Role">Role</el-tab-pane>
+      </el-tabs>
     </el-col>
   </el-row>
 </template>
 
 <script>
 import * as echarts from "echarts"
+import {
+  Plus,
+  Download,
+  Upload
+} from '@element-plus/icons-vue'
+
 
 export default {
   name: 'MainPage',
   data () {
     return {
-      chart: null
+      chart: null,
+      table_tab_name: String,
+
+      tableData: [
+        {
+          date: '2016-05-03',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles',
+          id: 0
+        },
+        {
+          date: '2016-05-02',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles',
+          id: 1
+        },
+        {
+          date: '2016-05-04',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles',
+          id: 2
+        },
+        {
+          date: '2016-05-01',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles',
+          id: 3
+        },
+        {
+          date: '2016-05-08',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles',
+          id: 4
+        },
+        {
+          date: '2016-05-06',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles',
+          id: 5
+        },
+        {
+          date: '2016-05-07',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles',
+          id: 6
+        },
+      ],
+      handleEdit: (index, row) => {
+        console.log(row.id)
+      },
+      handleDelete: (index, row) => {
+        console.log(index, row)
+      },
+      onAddClick: () => {
+        console.log('click add')
+      },
+      onAddSelect: type => {
+        console.log(type)
+      }
     }
+  },
+  components: {
+    Plus,
+    Download,
+    Upload
   },
   mounted () {
     this.chart = echarts.init(document.getElementById('main'))
@@ -39,6 +168,7 @@ export default {
   },
   unmounted () {
     this.chart.dispose()
-  }
+  },
+
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import App from './App.vue'
 
-createApp(App)
-    .use(ElementPlus)
-    .mount('#app')
+const app = createApp(App);
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+app.use(ElementPlus);
+app.mount("#app");
