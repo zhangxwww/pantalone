6a4eea4c7a236ccde8c433379070d680ded667f9
zhangxwww <631133544@qq.com>
2024-05-16 16:56:36 +0800

    init

---
+++ electron/main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow () {
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 900,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    });
+
+    win.loadURL('http://localhost:9876');
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
