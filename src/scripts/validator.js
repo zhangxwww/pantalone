7ce3fe29d298fc8710a5fec2e612d36361d11a45
zhangxwww <631133544@qq.com>
2024-06-12 14:04:17 +0800

    fix: 0 is valid number

--- src/scripts/validator.js
+++ src/scripts/validator.js
@@ -5,6 +5,8 @@ function isNumberValidator (rule, value, callback) {
         const regPositiveRealNum = /^(([1-9]\d*)|([0][.]{1}[0-9]{0,2}[1-9]+)|([1-9]\d*[.]{1}[0-9]+))$/g;
         if (regPositiveRealNum.test(value)) {
             callback();
+        } else if (parseInt(value) === 0) {
+            callback();
         } else {
             return callback(new Error(rule.message));
         }
