6d56f546644bd0409a3de60035d9008b20cacaea
zhangxwww <631133544@qq.com>
2024-08-31 16:28:33 +0800

    change db file path

--- backend/sql_app/database.py
+++ backend/sql_app/database.py
@@ -1,8 +1,17 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+
+def get_db_path():
+    db_dir = os.path.join(os.path.expanduser('~'), 'pantalone', 'db')
+    if not os.path.exists(db_dir):
+        os.makedirs(db_dir)
+    return os.path.join(db_dir, 'pantalone.db')
+
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{get_db_path()}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
