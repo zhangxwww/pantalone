a377658475b2a374b41d5ac399341da02009c910
zhangxwww <631133544@qq.com>
2024-11-13 22:39:25 +0800

    optimize log request

--- backend/core/libs/utils/decorator.py
+++ backend/core/libs/utils/decorator.py
@@ -1,6 +1,7 @@
 import functools
 import time
 
+from pydantic import BaseModel
 from loguru import logger
 
 
@@ -19,8 +20,8 @@ def log_request(func):
     @functools.wraps(func)
     async def wrapper(*args, **kwargs):
         logger.info(f"Request: {func.__name__}")
-        logger.info(f"Args: {args}")
-        logger.info(f"Kwargs: {kwargs}")
+        logger.info(f"Args: {[arg for arg in args if isinstance(arg, BaseModel)]}")
+        logger.info(f"Kwargs: {[v for v in kwargs.values() if isinstance(v, BaseModel)]}")
         return await func(*args, **kwargs)
     return wrapper
 
