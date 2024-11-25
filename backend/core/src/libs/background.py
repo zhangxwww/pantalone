58321009f7926e47982ad5e879aa954f4f8840ee
zhangxwww <631133544@qq.com>
2024-11-25 23:59:27 +0800

    fix: background task cancel but pending

--- backend/core/src/libs/background.py
+++ backend/core/src/libs/background.py
@@ -1,7 +1,9 @@
+import asyncio
 from typing import Callable, List
 from datetime import datetime
 
 from sqlalchemy.ext.asyncio import AsyncSession
+from loguru import logger
 
 from libs.utils.date_transform import trans_str_date_to_next_n_trade_date, trans_date_to_str
 from libs.utils.asyncio import run_async_task
@@ -11,7 +13,10 @@ async def async_add_data_after_n_days_to_db(func: Callable, db: AsyncSession, da
     dates = [trans_str_date_to_next_n_trade_date(date, n) for date in dates]
     dates = [date for date in dates if date < datetime.now().date()]
     dates = [trans_date_to_str(date) for date in dates]
-    await func(db, dates)
+    try:
+        await func(db, dates)
+    except asyncio.CancelledError:
+        logger.warning(f"Task add_data_after_n_days_to_db {func.__name__} has been cancelled.")
 
 def add_data_after_n_days_to_db(func: Callable, db: AsyncSession, dates: List[str], n: int):
     run_async_task(async_add_data_after_n_days_to_db, func, db, dates, n)
