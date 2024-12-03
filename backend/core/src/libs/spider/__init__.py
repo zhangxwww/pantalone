d68ce7975b54268de7051d4269bf30f8d4953ccb
zhangxwww <631133544@qq.com>
2024-12-03 23:26:50 +0800

    add paths into config.yaml

--- backend/core/src/libs/spider/__init__.py
+++ backend/core/src/libs/spider/__init__.py
@@ -1,2 +1,3 @@
 from .raw import *
 from .ak import *
+from .sina import *
--- backend/core/src/libs/utils/path.py
+++ backend/core/src/libs/utils/path.py
@@ -1,15 +1,37 @@
 import os
 
+import yaml
+
+
+with open(os.path.join(os.getcwd(), '../..', 'config.yaml'), 'r') as f:
+    config = yaml.safe_load(f)['core']['paths']
+
+BASE_DIR = os.path.join(os.path.expanduser('~'), 'pantalone')
+if not os.path.exists(BASE_DIR):
+    os.makedirs(BASE_DIR)
+
 
 def get_log_file_path():
-    log_dir = os.path.join(os.path.expanduser('~'), 'pantalone', 'logs')
+    log_dir = config['logs']['dir']
+    log_dir = os.path.join(BASE_DIR, log_dir)
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
     return log_dir
 
-
 def get_db_path():
-    db_dir = os.path.join(os.path.expanduser('~'), 'pantalone', 'db')
+    db_dir = os.path.join(BASE_DIR, config['db']['dir'])
     if not os.path.exists(db_dir):
         os.makedirs(db_dir)
-    return os.path.join(db_dir, 'pantalone.db')
+    return os.path.join(db_dir, config['db']['name'])
+
+def get_temp_path():
+    temp_dir = os.path.join(BASE_DIR, config['temp']['dir'])
+    if not os.path.exists(temp_dir):
+        os.makedirs(temp_dir)
+    return temp_dir
+
+def get_docs_path():
+    docs_dir = os.path.join(BASE_DIR, config['docs']['dir'])
+    if not os.path.exists(docs_dir):
+        os.makedirs(docs_dir)
+    return docs_dir
