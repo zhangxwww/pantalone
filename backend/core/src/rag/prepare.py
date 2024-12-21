f2578f69aedffec41e71090af1c3115e304ab77d
zhangxwww <631133544@qq.com>
2024-12-21 22:08:22 +0800

    prepare argparse

--- backend/core/src/rag/prepare.py
+++ backend/core/src/rag/prepare.py
@@ -5,19 +5,19 @@ from rag.retriever.retriever import Retriever
 
 
 def prepare(args):
-    if args.crawl:
+    if args.crawl or args.all:
         crawler = Crawler()
         crawler.crawl()
 
-    if args.parse:
+    if args.parse or args.all:
         parser = Parser()
         parser.parse()
 
-    if args.clean:
+    if args.clean or args.all:
         cleaner = Cleaner()
         cleaner.clean()
 
-    if args.store:
+    if args.store or args.all:
         retriever = Retriever(args.store_batch_size)
         store_num = args.store_num if args.store_num != -1 else None
         retriever.update(store_num)
@@ -27,6 +27,7 @@ if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser()
+    parser.add_argument('--all', action='store_true')
     parser.add_argument('--crawl', action='store_true')
     parser.add_argument('--parse', action='store_true')
     parser.add_argument('--clean', action='store_true')
